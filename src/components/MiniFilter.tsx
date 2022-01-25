99888ed4936659cb6476d0e3e96f9990a74d6e6d
Andres Mejias <aemabit@gmail.com>
2022-01-25 11:30:56 -0300

    satisfy eslint

--- src/components/MiniFilter.tsx
+++ src/components/MiniFilter.tsx
@@ -1,6 +1,4 @@
 import * as React from "react";
-import IconButton from "@mui/material/IconButton";
-import Tooltip from "@mui/material/Tooltip";
 import { Box, Typography } from "@mui/material";
 
 export type MiniFilterProps = {
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -10,8 +10,6 @@ import Divider from "@mui/material/Divider";
 import ListItem from "@mui/material/ListItem";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
 import { Avatar, IconButton } from "@mui/material";
 import { OfflineBolt } from "@mui/icons-material";
 
